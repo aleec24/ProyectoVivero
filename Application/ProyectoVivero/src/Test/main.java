aed0d3929c1cdedefab6a39c9d863334c337e541
TribalRope <67075796+TribalRope@users.noreply.github.com>
2022-08-17 23:05:43 -0600

    Nuevo Commit
    This reverts commit 48b03ff4298f5b53153b1f55f763fcb7bd1cb94f.

---
+++ Application/ProyectoVivero/src/Test/main.java
@@ -0,0 +1,44 @@
+package Test;
+//referencias para la conexción
+import java.io.*;
+import java.sql.*;
+import javax.swing.*;
+
+
+public class main {
+        static Connection conn=null;
+    static Statement st=null;
+    static ResultSet rs=null;
+    
+ 
+    
+    static String bd="orcl";
+    static String login="prueba";
+    static String password="1234";
+    static String url="jdbc:oracle:thin:@localhost:1521:orcl";
+    
+    public static Connection Enlace(Connection conn)throws SQLException    {
+        try {
+         Class.forName("oracle.jdbc.OracleDriver");
+         conn=DriverManager.getConnection(url, login, password);
+        }
+        catch(ClassNotFoundException e )
+        {
+            System.out.print("Clase no encontrada");
+        }
+        return conn;
+    }
+    
+    public static Statement sta(Statement st)throws SQLException    {
+        conn=Enlace(conn);
+        st=conn.createStatement();
+        return st;
+    }
+    public static ResultSet EnlEst(ResultSet rs)throws SQLException    {
+       st=sta(st);
+       rs=st.executeQuery("select * from Cliente");
+        return rs;
+    }
+
+    
+}//fin main
