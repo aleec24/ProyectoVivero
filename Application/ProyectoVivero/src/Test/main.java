aee400a9a06c28d676ddf9fe2355fc2dd2b677be
aleec24 <78573991+aleec24@users.noreply.github.com>
2022-08-21 22:16:10 -0600

    Revert "Actualizacion Pantalla de Inicio"
    This reverts commit 891aae7e8c246223c43d48503d896f8dc028a6a3.

--- Application/ProyectoVivero/src/Test/main.java
+++ Application/ProyectoVivero/src/Test/main.java
@@ -6,7 +6,7 @@ public class main {
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Menu().setVisible(true);
+                new FinsertarCliente().setVisible(true);
             }
         });
     }
