28e6298db9d946170a9210c95e894c67325c1e78
aleec24 <78573991+aleec24@users.noreply.github.com>
2022-08-21 22:17:00 -0600

    Actualizacion Pantalla de Inicio x2

--- Application/ProyectoVivero/src/Test/main.java
+++ Application/ProyectoVivero/src/Test/main.java
@@ -6,7 +6,7 @@ public class main {
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FinsertarCliente().setVisible(true);
+                new Menu().setVisible(true);
             }
         });
     }
